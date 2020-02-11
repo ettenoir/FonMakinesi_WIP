c5d2b510de178d37309f7758083142ab53ff30cf
tufan <tufan.yilmaz@stu.bahcesehir.edu.tr>
2020-02-12 00:27:24 +0300

    initial commit

---
+++ Assets/Scripts/Blow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blow : MonoBehaviour
+{
+    public GameObject BlowerObject;
+    public Vector3 Ball;
+    public Vector3 Blower;
+    public int force;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Blower = BlowerObject.transform.position;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        other.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        Debug.Log("calisiyom");
+        Ball = other.transform.position;
+        Vector3 rDirection = other.transform.position - Blower;
+        Vector3 nDirection = rDirection.normalized;
+        other.GetComponent<Rigidbody>().AddForce(nDirection * force);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("ball")){
+            other.GetComponent<Debug2>().didSlowed = true;
+        }
+        other.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionZ;
+        other.transform.position = new Vector3(other.transform.position.x, other.transform.position.y, 0);
+    }
+}
+
---
+++ Assets/Scripts/Debug2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debug2 : MonoBehaviour
+{
+
+    public bool didSlowed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(didSlowed){
+            SlowDown();
+        }
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(transform.position, forward, Color.green);
+    }
+
+    void SlowDown(){
+        didSlowed = false;
+        gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
+    }
+}
